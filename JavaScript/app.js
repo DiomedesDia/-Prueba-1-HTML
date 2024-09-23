70e13368f730b043af9d55f20aab6bc623866935
Stephen Vargas Martinez <stephenvargasmartinez@Stephens-Mac-mini.local>
2024-09-23 14:02:50 -0500

    JavaScript

---
+++ JavaScript/app.js
@@ -0,0 +1,37 @@
+ console.log("Hola Mundo");
+/*
+ //Variables
+ let num1=39;
+ let num2=45;
+/*
+let pi=3.14156;
+
+let direccion="CALLE 11 #40-60";
+
+let estado=true;
+
+ console.log("num1");
+console.log("num 2");
+console.log("pi");
+console.log("estado");
+console.log("direccion");
+suma=num1+num2;
+console.log("suma",suma);
+//Concatenar valores
+console.log('el primer valor es: '+num1);
+console.log('el segundo valor es: '+num2);
+console.log('La summa es: '+suma);
+
+let nu1=parseIntprompt(("Ingerese el primer valor"));
+let nu2=parseIntprompt(("Ingerese el segundo valor"));
+
+console.log ('El primer valor ingresado es:'+$nu1, 'y el segundo valor es'+$nu2);
+*/
+let s= nu1+nu2;
+console.log("La suma es: "+s);
+//Generación de numeros aleatorios
+let aleatorios=Math.floor(Math.random()*50)+1;
+console.log("El numero aleatorio es: "+aleatorios);
+//Generación de numeros aleatorios entre 1 y 10
+let=aleatorios1=  Math.floor(Math.random()*100);
+console.log("El numero aleatorio es: "+aleatorios1);
